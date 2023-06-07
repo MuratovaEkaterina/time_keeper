2a62686450e3f4f680950bac986ded952672023b
MuratovaEkaterina <muratova.ekaterina.13@gmail.com>
2023-06-07 14:19:03 +0300

    05.06.2023

--- time_keeper/time_keeper/urls.py
+++ time_keeper/time_keeper/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home/', include('APP.urls')),
+    path('/', include('tasks.urls')),
     path('admin/', admin.site.urls),
     
 ]
